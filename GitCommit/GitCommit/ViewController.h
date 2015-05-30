fec2b6cb77c3f8a12c6cca747818ed83bbb2e360
caoboyan <macbookpro@MacBookdeMacBook-Pro.local>
2015-05-30 09:22:13 +0800

    first commit

---
+++ GitCommit/GitCommit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitCommit
+//
+//  Created by MacBook Pro on 15/5/30.
+//  Copyright (c) 2015年 demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitCommit/GitCommit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitCommit
+//
+//  Created by MacBook Pro on 15/5/30.
+//  Copyright (c) 2015年 demo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor orangeColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
